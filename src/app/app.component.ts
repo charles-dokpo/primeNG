18375e7d94e9d87504db62a5fb6fb585376a06f6
charles.dokpo <charles.dokpo@cgi.com>
2020-07-13 10:43:39 +0200

    add planning component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SelectItem} from 'primeng/api';
+
+interface Cars {
+  label: string ;
+  value: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'primeNG';
+
+  date1: Date;
+  date2: Date;
+
+  cars: Cars[];
+
+  selectedCars1: Cars[] = [];
+
+  ngOnInit() {
+
+    this.cars = [
+      {label: 'Audi', value: 'Audi'},
+      {label: 'BMW', value: 'BMW'},
+      {label: 'Fiat', value: 'Fiat'},
+      {label: 'Ford', value: 'Ford'},
+      {label: 'Honda', value: 'Honda'},
+      {label: 'Jaguar', value: 'Jaguar'},
+      {label: 'Mercedes', value: 'Mercedes'},
+      {label: 'Renault', value: 'Renault'},
+      {label: 'VW', value: 'VW'},
+      {label: 'Volvo', value: 'Volvo'},
+  ];
+
 }
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanningComponent } from './planning/planning.component';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import {ListboxModule} from 'primeng/listbox';
+import {ButtonModule} from 'primeng/button';
+import {MultiSelectModule} from 'primeng/multiselect';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanningComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    CalendarModule,
+    ListboxModule,
+    ButtonModule,
+    MultiSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
